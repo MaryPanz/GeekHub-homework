6f61097a08b83feb28814a7495e11191c3f41890
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-08 21:20:02 +0300

    Update task_1.py

--- HT_1/task_1.py
+++ HT_1/task_1.py
@@ -1,1 +1,9 @@
+"""Write a script which accepts a sequence of comma-separated numbers from user and generates a list and a tuple with those numbers."""
 
+# splits a str into a list
+print("This is my example: 1,3,5")
+
+my_numbers = input("Please, print numbers separated by commas: ").split(",")
+
+print("This is my list: ", my_numbers)
+print("This is my tuple: ", tuple(my_numbers))
