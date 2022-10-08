b1189218fc0a732401feb3d13170c23cfd36999e
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-08 22:07:50 +0300

    Update task_5.py

--- HT_1/task_5.py
+++ HT_1/task_5.py
@@ -1,1 +1,9 @@
+"""Write a script which accepts a decimal number from user and converts it to hexadecimal."""
 
+my_decimal = input("Please enter a decimal number: ")
+
+if my_decimal.isdecimal():
+    my_hex = hex(int(my_decimal))
+    print("This is a hexadecimal from your decimal:", my_hex)
+else:
+    print("That was not a decimal number!")
