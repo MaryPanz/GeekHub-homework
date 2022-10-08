53e2d6e6e1820059914c4e2d399381c9e91a59ba
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-08 21:44:55 +0300

    Update task_3.py

--- HT_1/task_3.py
+++ HT_1/task_3.py
@@ -1,1 +1,7 @@
+"""Write a script which accepts a <number> from user and print out a sum of the first <number> positive integers."""
 
+my_number = int(input("Please, print one number: "))
+my_sum = 0
+for i in range(my_number + 1):
+    my_sum = my_sum + i
+print(f"This is my sum: {my_sum}!")
