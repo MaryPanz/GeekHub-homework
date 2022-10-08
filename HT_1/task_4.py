85ed7436fc43712c88dba93f7739761e51bc484b
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-08 21:58:36 +0300

    Update task_4.py

--- HT_1/task_4.py
+++ HT_1/task_4.py
@@ -1,1 +1,10 @@
+"""Write a script which accepts a <number> from user and then <number> times asks user for string input.
+At the end script must print out result of concatenating all <number> strings."""
 
+my_number = int(input("Please, print one number: "))
+my_sentence_sum = ""
+for i in range(my_number):
+    my_sentence = input("Type your sentence here: ")
+    my_sentence_sum = my_sentence_sum + my_sentence + " "
+    
+print(f"This is the sum of all your sentences: {my_sentence_sum}")
