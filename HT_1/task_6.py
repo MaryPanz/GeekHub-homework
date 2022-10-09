bed644dca27fdaa00c0efb084870a28e47dcf8e8
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-09 23:36:22 +0300

    Update task_6.py

--- HT_1/task_6.py
+++ HT_1/task_6.py
@@ -2,20 +2,10 @@
 """Write a script to check whether a value from user input is contained in a group of values.
     e.g. [1, 2, 'u', 'a', 4, True] --> 2 --> True
          [1, 2, 'u', 'a', 4, True] --> 5 --> False"""
-         
-my_value = ""
+
 my_value = input("Type a value: ")
 my_list0 = [1, 2, 3, "u", "a", True]
 
-# numbers
-if my_value.isdecimal():
-    int_value = (int(my_value))
-    print(int_value in my_list0)
-# letters
-elif my_value in my_list0:
-    print(my_value in my_list0)
-# T/F
-elif (my_value == str(my_list0[5])):
-    print(True)
-else:
-    print(False)
+my_str = ""
+my_str = "".join([str(i) for i in my_list0])
+print(my_value in my_str)
