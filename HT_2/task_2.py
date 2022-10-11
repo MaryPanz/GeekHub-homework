2d077d57a29e3fd7e3e7a6a0083ee722f421ce85
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-12 00:00:39 +0300

    Update task_2.py

--- HT_2/task_2.py
+++ HT_2/task_2.py
@@ -1,1 +1,6 @@
 """Write a script to remove empty elements from a list."""
+Test list: [(), ('hey'), 
+            ('',), ('ma', 'ke', 'my'), 
+            [''], {}, ['d', 'a', 'y'], 
+            '', []
+           ]
