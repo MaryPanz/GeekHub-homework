3687ed4040558f7d5110016cfbde78c688b73336
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-12 15:17:53 +0300

    Update task_5.py

--- HT_2/task_5.py
+++ HT_2/task_5.py
@@ -1,2 +1,10 @@
 """Write a script to remove values duplicates from dictionary. 
 Feel free to hardcode your dictionary."""
+my_dict = {"cat": 5, "dog": 2, "rabbit": 2, "cows": 5} 
+new_dict = {}
+my_list = []
+for key, value in my_dict.items():
+    if value not in my_list:
+        my_list.append(value)
+        new_dict[key] = value
+print(new_dict)
