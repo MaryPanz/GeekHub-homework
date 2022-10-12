a120c00c9f8114fcdb81b05cb17dca1032d6644f
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-12 14:13:36 +0300

    Update task_2.py

--- HT_2/task_2.py
+++ HT_2/task_2.py
@@ -1,6 +1,4 @@
 """Write a script to remove empty elements from a list."""
-Test list: [(), ('hey'), 
-            ('',), ('ma', 'ke', 'my'), 
-            [''], {}, ['d', 'a', 'y'], 
-            '', []
-           ]
+my_list = [(), ('hey'), ('',), ('ma', 'ke', 'my'), [''], {}, ['d', 'a', 'y'], '', []]
+new_list = list(filter(None, my_list))
+print("This is my new list: ", new_list)
