58752c139fdc7c528554338a40d06532c859f6cf
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-12 14:43:41 +0300

    Update task_3.py

--- HT_2/task_3.py
+++ HT_2/task_3.py
@@ -3,3 +3,8 @@
 dict_1 = {'foo': 'bar', 'bar': 'buz'}
 dict_2 = {'dou': 'jones', 'USD': 36}
 dict_3 = {'AUD': 19.2, 'name': 'Tom'}
+dict_4 = {}
+for i in (dict_1, dict_2, dict_3):
+    dict_4.update(i)
+
+print("This is my updated dictionary: ", dict_4)
