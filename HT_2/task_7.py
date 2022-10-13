794fc17aa309eeb0d43e29a689794aa1431e3751
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-13 14:32:34 +0300

    Update task_7.py

--- HT_2/task_7.py
+++ HT_2/task_7.py
@@ -1,3 +1,8 @@
 """Write a script which accepts a <number>(int) from user and generates dictionary in range <number> where key is <number> and value is <number>*<number>
-
     e.g. 3 --> {0: 0, 1: 1, 2: 4, 3: 9}"""
+my_number = int(input("Type a number: "))
+my_dict = {}
+for i in range(my_number, my_number + 4):
+    my_dict[i] = i**2
+
+print("This is my dictionary:", my_dict)
