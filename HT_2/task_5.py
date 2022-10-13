b9882e5da9804609acf35679b5286db66764b5b5
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-13 17:43:04 +0300

    Update task_5.py

--- HT_2/task_5.py
+++ HT_2/task_5.py
@@ -2,9 +2,8 @@
 Feel free to hardcode your dictionary."""
 my_dict = {"cat": 5, "dog": 2, "rabbit": 2, "cows": 5} 
 new_dict = {}
-my_list = []
+
 for key, value in my_dict.items():
-    if value not in my_list:
-        my_list.append(value)
+    if value not in new_dict.values():
         new_dict[key] = value
 print(new_dict)
