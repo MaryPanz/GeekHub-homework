3408fbd35ba8b8e55f97bf1da98dec95bb916973
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-13 14:19:11 +0300

    Update task_6.py

--- HT_2/task_6.py
+++ HT_2/task_6.py
@@ -1,1 +1,6 @@
 """Write a script to get the maximum and minimum VALUE in a dictionary."""
+my_dict = {"cookies": 25, "bread": 1, "carrots": 3, "cakes": 2}
+only_value = my_dict.values()
+print("This is my dictionary:", my_dict)
+print("This is the maximum value:", (max(only_value)))
+print("This is the minimum value:", (min(only_value)))
