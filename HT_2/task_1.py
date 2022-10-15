e3adba04efd8f8e2466e7ab76a333790b9add251
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-15 10:58:06 +0000

    Update task_1.py

--- HT_2/task_1.py
+++ HT_2/task_1.py
@@ -2,13 +2,9 @@
 The list of tuples can be hardcoded. The "replacement" value is entered by user. 
 The number of elements in the tuples must be different."""
 
-my_tuple_list = [(1,2,3), (4,5), (6,7,8,9), (), ()]
+my_tuple_list = [(1,2,3), (), (4,5), (6,7,8,9), (), ()]
 
 my_value = input("Please, write your value: ")
-my_new_list = [i[:-1] + (my_value,) for i in my_tuple_list if i ]
-
-for i in my_tuple_list:
-    if i == ():
-        my_new_list.append(())
+my_new_list = [i[:-1] + (my_value,) if i else i for i in my_tuple_list]
     
 print("This is my new list: ", my_new_list)
