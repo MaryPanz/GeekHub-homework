4f9f9f9a9ffd68cd81bd1bd8d26485af56874aa5
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-16 22:22:20 +0300

    Update task_4.py

--- HT_4/task_4.py
+++ HT_4/task_4.py
@@ -12,7 +12,7 @@ def write_length(s):
     if len(s) > 29 and len(s) < 51:
         numbers = sum(i.isdigit() for i in s)
         letters = sum(i.isalpha() for i in s)
-        print(f"Length: {len(s)}. Letters: {(letters)}. Numbers: {(numbers)}")
+        print(f"Length: {len(s)}. Letters: {letters}. Numbers: {numbers}")
     elif len(s) < 31:
         sum_all = 0
         all_letters = ""
