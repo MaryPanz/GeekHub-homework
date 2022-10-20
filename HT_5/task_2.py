153b2e5adee72008069a982f1b03af86cb5b5092
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-20 17:01:19 +0300

    Update task_2.py

--- HT_5/task_2.py
+++ HT_5/task_2.py
@@ -8,6 +8,7 @@ money = int(input("Money: "))
 years = int(input("Years: "))
 
 def bank(money, years, percent = 0.1):
+    
     try:
         for i in range(years):
             result = "{:.2f}".format(money * (pow((1 + percent), 1 * (i + 1))))
