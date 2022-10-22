5fdd59cd9c162cc624cc293b6b2e9532e70a2f84
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-22 20:32:02 +0000

    Create task_2.py

---
+++ HT_6/task_2.py
@@ -0,0 +1,20 @@
+name = input("Name: ")
+password = input("Password: ")
+
+
+def validate(name, password):
+    
+    if len(name) > 5:
+        print("The name must be smaller than 50 symbols")
+    if len(name) < 3:
+        print("The name must be bigger than 2 symbols")
+    if len(password) < 8:
+        print("The password can't be less than 8 digits")
+    if not any(i.isdigit() for i in password):
+        print("The password should have a number")
+    if not any(i.isupper() for i in password):
+        print("The password should have an uppercase letter")
+    else:
+        print("Valid!")
+    
+(validate(name, password))
