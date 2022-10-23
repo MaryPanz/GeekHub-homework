88df7320f85262f6c91e90d8396ed6afbf5bc8a4
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-23 10:53:23 +0000

    Update task_4.py

--- HT_6/task_4.py
+++ HT_6/task_4.py
@@ -1,8 +1,8 @@
 morse = input("Morse: ")
-
+# -.-. .- -   .. ...   ..-. .- -
 
 def morse_code(morse):
-    my_dict = { '.-': 'A', '-...': 'B':=,
+    my_dict = { '.-': 'A', '-...': 'B',
    '-.-.': 'C', '-..': 'D', '.': 'E',
    '..-.': 'F', '--.': 'G', '....': 'H',
    '..': 'I', '.---': 'J', '-.-': 'K',
@@ -10,14 +10,17 @@ def morse_code(morse):
    '---': 'O', '.--.': 'P', '--.-': 'Q',
    '.-.': 'R', '...': 'S', '-': 'T',
    '..-': 'U', '...-': 'V', '.--': 'W',
-   '-..-': 'X', '-.--': 'Y', '--..': 'Z'}
-   
-    sentence = morse.split(" ")
-    english = []  # Will contain English versions of letters
+   '-..-': 'X', '-.--': 'Y', '--..': 'Z', '*': ' '}
+    
+    s = morse.replace('   ', ' * ')
+    
+    sentence = s.split(" ")
+    
+    english = [] 
     for i in sentence:
         if i in my_dict:
             english.append(my_dict[i])
-    return " ".join(english)
+    return "".join(english)
     
     
     
