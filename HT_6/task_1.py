993e17ab94492683f2b373fc0a0a5c2f585a70ba
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-24 10:57:01 +0300

    Update task_1.py

--- HT_6/task_1.py
+++ HT_6/task_1.py
@@ -9,27 +9,25 @@
 
 class LoginException(Exception):
     pass
-  
+
 
 username = input("Your username: ")
 password = input("Your password: ")
 
 
-def my_users(username, password, silent = False):
-    
+def my_users(username, password, silent=False):
     my_users_list = [["david", "1234"], ["kate", "2345"], ["leo", "1234"], ["bob", "4342"], ["emily", "5454"]]
     user_pass = [username, password]
-    
-    try:    
+
+    try:
         if user_pass in my_users_list:
             silent = True
             if silent:
                 print(False)
         else:
             raise LoginException
-            
     except LoginException:
         print("Your username/password is incorrect!")
-    
-            
-(my_users(username, password, silent = False))
+
+
+my_users(username, password, silent=False)
