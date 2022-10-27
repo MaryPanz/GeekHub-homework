3d73b5cc423dcd66f88e4edb281f1dd14d510d91
Mariia Panzhynska <marypanz90@gmail.com>
2022-10-27 15:07:39 +0300

    Update task_6.py

--- HT_7/task_6.py
+++ HT_7/task_6.py
@@ -10,7 +10,6 @@ def short_word_length(sentence):
 
     my_list = list(sentence.split(" "))
     result = min((word for word in my_list if word), key=len)
-
     print("The length of the shortest word '%s' is %s!" % (result, len(result)))
 
 short_word_length(sentence)
